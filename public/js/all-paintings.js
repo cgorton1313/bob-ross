2aad336c31219f09926d8e06e1dd9bfe7f4bce8e
mgilberg <mgilberg25@penguinhall.org>
2022-05-11 10:14:15 -0400

    new edits

--- public/js/all-paintings.js
+++ public/js/all-paintings.js
@@ -1,66 +1,21 @@
-insertTableInfo("Painting ID");
-insertTableInfo("Bob Ross");
-insertTableInfo("a");
-insertTableInfo("b");
-insertTableInfo("c");
-insertTableInfo("d");
-insertTableInfo("e");
-insertTableInfo("f");
+insertTableInfo("a","1","2","3","4","5","6","7");
+insertTableInfo("b","1","2","3","4","5","6","7");
 
-
-function insertTableInfo(a) {
-    const newNode = document.createElement("td");
-    const textNode = document.createTextNode(a);
-    newNode.appendChild(textNode);
-
-    const list = document.getElementById("myTable");
-    list.insertBefore(newNode, list.children[0]);
+function insertTableInfo(info1,info2,info3,info4,info5,info6,info7) {
+    var table = document.getElementById("paintingsTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    cell1.innerHTML = info1;
+    cell2.innerHTML = info2;
+    cell3.innerHTML = info3;
+    cell4.innerHTML = info4;
+    cell5.innerHTML = info5;
+    cell6.innerHTML = info6;
+    cell7.innerHTML = info7;
 }
-
-// remember not to push any of the work
-// here is the whole example (what I found). We only need to use the script part but I added everthing just in case
-// <!DOCTYPE html>
-//{/* <html>
-{/* <head>
-<style>
-table, td {
-  border: 1px solid black;
-}
-</style>
-</head>
-<body>
-
-<p>Click the button to add a new row at the first position of the table and then add cells and content.</p>
-
-<table id="myTable">
-  <tr>
-    <td>Row1 cell1</td>
-    <td>Row1 cell2</td>
-  </tr>
-  <tr>
-    <td>Row2 cell1</td>
-    <td>Row2 cell2</td>
-  </tr>
-  <tr>
-    <td>Row3 cell1</td>
-    <td>Row3 cell2</td>
-  </tr>
-</table>
-<br>
-
-<button type="button" onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
-}
-</script>
-
-</body>
-</html> */} 
-
