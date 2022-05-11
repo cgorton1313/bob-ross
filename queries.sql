09bc83e98b72e889a8f22017d5e98ca143ef3d49
KRiley <kriley24@penguinhall.org>
2022-05-11 10:52:20 -0400

    update

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
+--replace # with desired number
+
 SELECT * FROM BobRossData WHERE painting_index = #
--- to get all data by the painting's id
-SELECT img_src, painting_title, painting_index From BobRossData
--- to get painting img and title
+ORDER BY episode asc, season asc
+-- to get all data by the painting's id. Ordered by season and episode.
+SELECT img_src, painting_title, painting_index, episode, season From BobRossData
+ORDER BY season asc, episode asc
+-- to get painting's img, title, season, episode, and index. Ordered by season and episode.
 SELECT DISTINCT * FROM BobRossData WHERE season = #
 ORDER BY episode asc
--- to get paintings by season
+-- to get paintings by season. Ordered by episode.
