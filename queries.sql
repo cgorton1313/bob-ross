d3b64eae7e42dc9a2433016be75ad69ad7ccee5d
KRiley <kriley24@penguinhall.org>
2022-05-13 09:22:23 -0400

    update

--- queries.sql
+++ queries.sql
@@ -11,6 +11,57 @@ ORDER BY season asc, episode asc;
 SELECT DISTINCT * FROM BobRossData WHERE season = #
 ORDER BY episode asc;
 
--- to get paintings based off of the colors they have. Ex. Black Gesso
+-- to get paintings based off of the colors they have.
 SELECT * FROM BobRossData WHERE Black_Gesso = true
-ORDER BY season asc, episode asc
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Bright_Red = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Burnt_Umber = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Cadium_Yellow = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Dark_Sienna = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Indian_Red = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Indian_Yellow = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Liquid_Black = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Liquid_Clear = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Midnight_Black = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Phthalo_Blue = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Phthalo_Green = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Prussian_Blue = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Sap_Green = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Titanium_White = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Van_Dyke_Brown = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Yellow_Ochre = true
+ORDER BY season asc, episode asc;
+
+SELECT * FROM BobRossData WHERE Alizarin_Crimson = true
+ORDER BY season asc, episode asc;
