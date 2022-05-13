2b5f4833cbce816811b022fd7bbb5868989329c8
nathalieforsythe <nforsythe22@penguinhall.org>
2022-05-13 09:19:36 -0400

    adding paintings endpoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // requires
-const express = require('express'); 
+const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -10,9 +10,14 @@ app.use(express.static(path.join(__dirname + '/public'))); // send all the stati
 // homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
-    });
+});
+
+const paintingData = [{ painting_index: 283, season: 1, episode: 2, painting_title: 'Mt. McKinley' }, { painting_index: 284, season: 1, episode: 3, painting_title: 'Ebony Sunset' }];
+app.get('/paintings', async function (req, res) {
+    res.json(paintingData);
+});
 
 // goes at the end
 app.listen(config.app.port, () => {
     console.log(`Bob Ross server listening at http://localhost:${config.app.port}`);
-    });
+});
