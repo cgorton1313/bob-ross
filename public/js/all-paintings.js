403c04517d4e7363d8c74a6d4c73b0d11d058693
mgilberg <mgilberg25@penguinhall.org>
2022-05-13 09:07:39 -0400

    new edit

--- public/js/all-paintings.js
+++ public/js/all-paintings.js
@@ -1,27 +1,27 @@
 
 const paintingData = [{painting_index: 283, season: 1, episode: 2, painting_title: 'Mt. McKinley'},{painting_index: 284, season: 1, episode: 3, painting_title: 'Ebony Sunset'}];
 
-// image src, painting title, season, episode, num-colors, youtiube src, colors
+// image src, painting title, season, episode, num-colors, youtube src, colors
 
 for (let i = 0; i < paintingData.length; i++) {
-  insertTableInfo((i + 1), "a", paintingData[i].painting_title, paintingData[i].season, paintingData[i].episode, "4", "5", "6", "7");
+  insertTableInfo((i + 1), "a", paintingData[i].painting_title, paintingData[i].season, paintingData[i].episode, "4", "5", "6");
 }
 
-function insertTableInfo(rowNum, info1, info2, info3, info4, info5, info6, info7) {
+function insertTableInfo(rowNum, imgSource, pntTitle, season, episode, numClrs, ytSource, colors) {
   var table = document.getElementById("paintingsTable");
   var row = table.insertRow(rowNum);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
-  var cell6 = row.insertCell(5);
-  var cell7 = row.insertCell(6);
-  cell1.innerHTML = info1;
-  cell2.innerHTML = info2;
-  cell3.innerHTML = info3;
-  cell4.innerHTML = info4;
-  cell5.innerHTML = info5;
-  cell6.innerHTML = info6;
-  cell7.innerHTML = info7;
+  var imgSourceCell = row.insertCell(0);
+  var pntTitleCell = row.insertCell(1);
+  var seasonCell = row.insertCell(2);
+  var episodeCell = row.insertCell(3);
+  var numClrsCell = row.insertCell(4);
+  var ytSourceCell = row.insertCell(5);
+  var colorsCell = row.insertCell(6);
+  imgSourceCell.innerHTML = imgSource;
+  pntTitleCell.innerHTML = pntTitle;
+  seasonCell.innerHTML = season;
+  episodeCell.innerHTML = episode;
+  numClrsCell.innerHTML = numClrs;
+  ytSourceCell.innerHTML = ytSource;
+  colorsCell.innerHTML = colors;
 }
