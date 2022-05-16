d54a6da40ad5f8c3d8a2573b894e32dc20ae2cc1
nathalieforsythe <nforsythe22@penguinhall.org>
2022-05-16 10:25:55 -0400

    returned real data

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-const paintingData = [{ painting_index: 283, season: 1, episode: 2, painting_title: 'Mt. McKinley' }, { painting_index: 284, season: 1, episode: 3, painting_title: 'Ebony Sunset' }];
+// const paintingData = [{ painting_index: 283, season: 1, episode: 2, painting_title: 'Mt. McKinley' }, { painting_index: 284, season: 1, episode: 3, painting_title: 'Ebony Sunset' }];
 app.get('/paintings', async function (req, res) {
-    res.json(paintingData);
-    // res.json (await bobRossData.getPaintings());
+    // res.json(paintingData);
+    res.json (await bobRossData.getPaintings());
 });
 
 // always goes last
--- tests/db-tests.js
+++ tests/db-tests.js
@@ -2,10 +2,10 @@ const bobRossData = require('../bob-ross-data.js');
 
 async function testSomething() {
     // here you can call a function that is in bob-ross-data.js and wait for the result
-    let someData = await bobRossData.sqlTest();
-    // let paintingsData = await bobRossData.getPaintings();
-    console.log(someData);
-    // console.log(paintingsData);
+    // let someData = await bobRossData.sqlTest();
+    let paintingsData = await bobRossData.getPaintings();
+    // console.log(someData);
+    console.log(paintingsData);
 }
 
 testSomething(); // this was working? I haven't changed anything but when I tried it at home i couldn't get into the database
