4edd45668a38d1d700f2a654e0066578ba077b55
mgilberg <mgilberg25@penguinhall.org>
2022-05-17 11:03:08 -0400

    new edit

--- public/js/all-paintings.js
+++ public/js/all-paintings.js
@@ -5,11 +5,11 @@ async function getTableInfo() {
   let response = await fetch('./paintings');
   let paintingData = await response.json();
   for (let i = 0; i < 10; i++) {
-    insertTableInfo((i + 1), paintingData[i].img_src, paintingData[i].painting_title, paintingData[i].season, paintingData[i].episode, paintingData[i].colors, paintingData[i].youtube_src);
+    insertTableInfo((i + 1), paintingData[i].img_src, paintingData[i].painting_title, paintingData[i].painting_index, paintingData[i].season, paintingData[i].episode, paintingData[i].colors, paintingData[i].youtube_src);
   }
 }
 
-function insertTableInfo(rowNum, imgSource, pntTitle, season, episode,colors, ytSource ) {
+function insertTableInfo(rowNum, imgSource, pntTitle, pntIndex, season, episode,colors, ytSource ) {
   var table = document.getElementById("paintingsTable");
   var row = table.insertRow(rowNum);
   var imgSourceCell = row.insertCell(0);
@@ -19,7 +19,7 @@ function insertTableInfo(rowNum, imgSource, pntTitle, season, episode,colors, yt
   var ytSourceCell = row.insertCell(4);
   var colorsCell = row.insertCell(5);
   imgSourceCell.innerHTML = "<img src="+imgSource+"></img>";
-  pntTitleCell.innerHTML = "<a href='individual-episode.html'>"+pntTitle+"</a>";
+  pntTitleCell.innerHTML = "<a href='individual-episode.html?id="+pntIndex+"'>"+pntTitle+"</a>";
   seasonCell.innerHTML = season;
   episodeCell.innerHTML = episode;
   ytSourceCell.innerHTML = ytSource;
