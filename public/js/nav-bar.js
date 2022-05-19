baaca4adafe4697db1429488f256f4eff7490645
crietti <crietti22@penguinhall.org>
2022-05-19 09:33:21 -0400

    seaons are dynamic now!!

--- public/js/nav-bar.js
+++ public/js/nav-bar.js
@@ -10,46 +10,18 @@ let navHtml = `
                 class="w3-mobile w3-bar-item w3-button w3-hover-none w3-bottombar w3-border-white w3-hover-border-black"><strong>PAINTINGS<strong></a>
             <a href="about.html"
                 class="w3-mobile w3-bar-item w3-button w3-hover-none w3-bottombar w3-border-white w3-hover-border-black"><strong>ABOUT<strong></a>
+            
 
             <div
                 class="w3-right w3-dropdown-hover w3-mobile w3-hover-none w3-bottombar w3-border-white w3-hover-border-black">
                 <a href="seasons.html" class="w3-button">SEASONS <i class="fa fa-caret-down"></i></a>
-                <div id="dropDown" class="w3-dropdown-content w3-bar-block" style="max-height:250px; overflow:auto">
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 1</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 2</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 3</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 4</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 5</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 6</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 7</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 8</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 9</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 10</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 11</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 12</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 13</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 14</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 15</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 16</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 17</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 18</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 19</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 20</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 21</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 22</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 23</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 24</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 25</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 26</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 27</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 28</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 29</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 30</a>
-                    <a href="#" class="w3-bar-item w3-button w3-mobile">Season 31</a>
-                </div>
-            </div>
-        </div>
-    </div>
-`
+                <div id="dropDown" class="w3-dropdown-content w3-bar-block" style="max-height:250px; overflow:auto">`;
+
+                for (let i = 1; i <= 31; i++) {
+    navHtml += `<a href="season.html?season=` + i + `" class="w3-bar-item w3-button w3-mobile">Season ` + i + `</a>`
+}
+
+navHtml += `</div></div></div></div>`
+
 
 document.getElementById("navBar").innerHTML = navHtml
