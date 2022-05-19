eb49f2f3cb8b4e1c23ff4130429d61a42a676d85
nathalieforsythe <nforsythe22@penguinhall.org>
2022-05-19 08:42:48 -0400

    finished new logging system

--- tests/db-tests.js
+++ tests/db-tests.js
@@ -1,8 +1,15 @@
 const bobRossData = require('../bob-ross-data.js');
 
+const SimpleNodeLogger = require('simple-node-logger');
+const opts = {
+    logFilePath: config.app.logFile,
+    timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
+};
+const log = SimpleNodeLogger.createSimpleLogger(opts);
+
 async function testSomething() {
     let someData = await bobRossData.sqlTest();
-    console.log(someData);
+    log.info(someData);
 }
 
 testSomething();
