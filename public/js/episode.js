dc8ced794b6b79670b6a867ebb28af941b4a9e6b
mgilberg <mgilberg25@penguinhall.org>
2022-05-19 08:43:43 -0400

    new edit

--- public/js/episode.js
+++ public/js/episode.js
@@ -1,4 +1,22 @@
-// console.log("hi");
 const urlParams = new URLSearchParams(window.location.search);
 let id = parseInt(urlParams.get('id'));
-console.log(id);
+// console.log(id);
+
+
+
+getTableInfo();
+
+async function getTableInfo() {
+    let response = await fetch('./paintings');
+    let paintingData = await response.json();
+    for (let i = 0; i < paintingData.length; i++) {
+        insertEpisodeInfo(i);
+    }
+}
+async function insertEpisodeInfo(a) {
+    let response = await fetch('./paintings');
+    let paintingData = await response.json();
+    if (paintingData[a].painting_index == id) {
+        console.log(paintingData[a]);
+    }
+}
