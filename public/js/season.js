d475ca7ca21ec85293c6d374106f72fed77472ba
KRiley <kriley24@penguinhall.org>
2022-05-25 11:21:31 -0400

    update

---
+++ public/js/season.js
@@ -0,0 +1,25 @@
+const urlParams = new URLSearchParams(window.location.search);
+let id = parseInt(urlParams.get('season'));
+// let episodeRowHtml = `<div class="w3-row-padding">
+// <div class="w3-col m6 w3-margin-bottom">
+//     <div class="w3-display-container">
+//         <img src="https://htmlcolorcodes.com/assets/images/colors/pastel-purple-color-solid-background-1920x1080.png"
+//             alt="Purple" style="width:100%">
+
+//         <button class="w3-button w3-black w3-block w3-hover-purple">View episode</button>
+//     </div>
+// </div>
+// <h3 class="w3-opacity w3-center"><i>Episode Name</i></h3>
+// <p class="w3-opacity w3-center"><i>Episode Number</i></p>
+// <p class="w3-opacity w3-center"><i>Text discription</i></p>
+// </div>`
+
+insertEpisodeRow()
+
+async function insertEpisodeRow(){
+    let response = await fetch('season?season=' + id);
+    let paintingData = await response.json();
+    // document.getElementById("body").innerHTML = episodeRowHtml
+    console.log(id)
+    console.log(paintingData)
+}
